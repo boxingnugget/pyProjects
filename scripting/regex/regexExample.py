7d134f543aab28812cc2b63f51ff1b866e7e2ab7
boxingnugget <michaelnguyen98@gmail.com>
2020-11-16 00:00:14 -0500

    regexPractice

---
+++ scripting/regex/regexExample.py
@@ -0,0 +1,5 @@
+import re
+
+phoneNumberRegex = re.compile(r'((d{3}))(d{3}-d{4})')#creates regular expression object in the xxx-xxx-xxxx format
+matchedObject = phoneNumberRegex.search('test phone number (703)420-6969')#searches what is passed thru
+print(matchedObject.group())#returns phone numbers found
